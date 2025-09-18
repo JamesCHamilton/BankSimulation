913ef95108d15e8bd162428c1cfd055fe96b4e53
JamesCHamilton <165865052+JamesCHamilton@users.noreply.github.com>
2025-09-18 10:42:25 -0400

    Add files via upload

---
+++ TransactionSim/src/main/java/com/bankSim/TransactionSim/TransactionSimApplication.java
@@ -0,0 +1,13 @@
+package com.bankSim.TransactionSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSimApplication.class, args);
+	}
+
+}
---
+++ TransactionSim/src/test/java/com/bankSim/TransactionSim/TransactionSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankSim.TransactionSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
